13b8a5d95e334f6a5e6b42af4909d6e219673b30
Booker Zhao <bookerzhao@tencent.com>
2020-05-09 16:49:41 +0800

    feat(logger): deployer and builder base class

--- packages/framework-core/src/builder/index.ts
+++ packages/framework-core/src/builder/index.ts
@@ -1,21 +1,25 @@
-import { resolve } from 'path'
-import fs from 'fs-extra'
+import { resolve } from "path";
+import fs from "fs-extra";
+import getLogger, { Logger } from "../logger";
+
 interface BuilderOptions {
-    type: 'node' | 'static'
-    projectPath: string
+  type: "node" | "static";
+  projectPath: string;
 }
 export class Builder {
-    protected distDir: string
-    protected projectDir: string
-    protected distDirName: string
-    constructor(options: BuilderOptions) {
-        const { type, projectPath } = options
-        this.distDirName = `cloudbase-${type}-build-${new Date().getTime()}`
-        this.projectDir = projectPath
-        this.distDir = resolve(projectPath, this.distDirName)
-    }
+  protected distDir: string;
+  protected projectDir: string;
+  protected distDirName: string;
+  protected logger: Logger;
+  constructor(options: BuilderOptions) {
+    const { type, projectPath } = options;
+    this.distDirName = `cloudbase-${type}-build-${new Date().getTime()}`;
+    this.projectDir = projectPath;
+    this.distDir = resolve(projectPath, this.distDirName);
+    this.logger = getLogger();
+  }
 
-    async clean() {
-        return fs.remove(this.distDir)
-    }
+  async clean() {
+    return fs.remove(this.distDir);
+  }
 }
--- packages/framework-core/src/deployer/index.ts
+++ packages/framework-core/src/deployer/index.ts
@@ -1,9 +1,13 @@
 import { DeployerOptions } from "../types";
 import CloubaseManager from "@cloudbase/manager-node";
 
+import getLogger, { Logger } from "../logger";
+
 export class Deployer {
   protected app: CloubaseManager;
+  protected logger: Logger;
   constructor(options: DeployerOptions) {
     this.app = options.cloudbaseManager;
+    this.logger = getLogger();
   }
 }
