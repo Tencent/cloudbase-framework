baa61a60443e956dcd53df598d451809462daa40
Weijia Wang <starkwang@126.com>
2020-05-14 20:11:10 +0800

    static builder支持通配符

--- packages/cloudbase-static-builder/src/index.ts
+++ packages/cloudbase-static-builder/src/index.ts
@@ -1,45 +1,58 @@
 import path from 'path'
 import fs from 'fs-extra'
 import { Builder } from '@cloudbase/framework-core'
+import cpy from 'cpy'
+import anymatch from 'anymatch'
 
 interface StaticBuilderBuildOptions {
-    /**
-     * 云接入路径
-     */
-    path: string
+  /**
+   * 云接入路径
+   */
+  path?: string,
+  exclude?: string[]
 }
 
 interface StaticBuilderOptions {
-    /**
-     * 项目根目录的绝对路径
-     */
-    projectPath: string
+  /**
+   * 项目根目录的绝对路径
+   */
+  projectPath: string
 }
 
 export class StaticBuilder extends Builder {
   constructor(options: StaticBuilderOptions) {
     super({
-        type: 'static',
-        ...options
+      type: 'static',
+      ...options
     });
   }
-  async build(entry: string, options?: StaticBuilderBuildOptions) {
+  async build(entry: string, options: StaticBuilderBuildOptions = {}) {
     const fileList = await fs.readdir(entry)
+    const exclude = options.exclude || []
     for (const file of fileList) {
-      await fs.copy(path.resolve(entry, file), path.resolve(this.distDir, file))
+      await cpy(
+        [path.resolve(entry, file)],
+        this.distDir,
+        {
+          filter: () => {
+            const matchers = [...exclude]
+            return !anymatch(matchers, file);
+          }
+        }
+      )
     }
     return {
       static: [
         {
           src: this.distDir,
-          cloudPath: options ? options.path || '/' : '/',
+          cloudPath: options.path || '/'
         },
       ],
       routes: [
         {
-          path: options ? options.path || '/' : '/',
+          path: options.path || '/',
           targetType: 'static',
-          target: options ? options.path || '/' : '/',
+          target: options.path || '/'
         },
       ]
     };
