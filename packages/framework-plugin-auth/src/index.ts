4b9ca9f459d0872f911c8851c38cc5bce19489eb
Zijie Zhou <lt520c@gmail.com>
2020-09-27 16:15:37 +0800

    feat(auth): support auth plugin

---
+++ packages/framework-plugin-auth/src/index.ts
@@ -0,0 +1,104 @@
+import { Plugin, PluginServiceApi } from "@cloudbase/framework-core";
+
+/**
+ * 导出接口用于生成 JSON Schema 来进行智能提示
+ */
+export interface IFrameworkPluginAuthInputs {
+  configs: IAuthConfig[]
+}
+
+export interface IAuthConfig {
+  /**
+   * 登录平台
+   */
+  platform: "NONLOGIN" | "ANONYMOUS"
+  /**
+   * 开通状态
+   */
+  status: "ENABLE" | "DISABLE"
+  /**
+   * 登录平台的 AppID，选填
+   */
+  platformId: string
+  /**
+   * 登录平台的 AppSecret，选填
+   */
+  platformSecret: string
+}
+
+class AuthPlugin extends Plugin {
+  protected resolvedInputs: IFrameworkPluginAuthInputs;
+  protected buildOutput: any;
+
+  constructor(
+    public name: string,
+    public api: PluginServiceApi,
+    public inputs: IFrameworkPluginAuthInputs
+  ) {
+    super(name, api, inputs);
+
+    const DEFAULT_INPUTS = {
+      
+    };
+    this.resolvedInputs = resolveInputs(this.inputs, DEFAULT_INPUTS);
+  }
+
+  /**
+   * 初始化
+   */
+  async init() {
+    this.api.logger.debug("AuthPlugin: init", this.resolvedInputs);
+  }
+
+  /**
+   * 执行本地命令
+   */
+  async run() {}
+
+  /**
+   * 删除资源
+   */
+  async remove() {}
+
+  /**
+   * 生成代码
+   */
+  async genCode() {}
+
+  /**
+   * 构建
+   */
+  async build() {}
+
+  /**
+   * 生成SAM文件
+   */
+  async compile() {
+    this.api.logger.debug("AuthPlugin: compile", this.resolvedInputs, this.buildOutput);
+
+    return {
+      Config: {
+        Login: this.resolvedInputs.configs.map((config: IAuthConfig) => {
+          const { platform, status, platformId, platformSecret } = config;
+          return {
+            Platform: platform,
+            PlatformId: platformId,
+            PlatformSecret: platformSecret,
+            Status: status
+          }
+        })
+      }
+    };
+  }
+
+  /**
+   * 部署
+   */
+  async deploy() {}
+}
+
+function resolveInputs(inputs: any, defaultInputs: any) {
+  return Object.assign({}, defaultInputs, inputs);
+}
+
+export const plugin = AuthPlugin;
