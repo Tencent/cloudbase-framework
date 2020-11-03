9124baaf86564919241cf7a7ce312bbbb13615bd
Booker Zhao <bookerzhao@tencent.com>
2020-11-03 13:16:15 +0800

    ci(license):  license checker

--- scripts/license-checker.js
+++ scripts/license-checker.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
-const spawnPromise = require('./spawn');
+
+let allDeps = {};
+
+main();
 
 async function main() {
   await licenseCheck('main', process.cwd());
@@ -13,18 +16,29 @@ async function main() {
   for (let file of files) {
     await licenseCheck(file, path.join(process.cwd(), 'packages', file));
   }
+
+  let report = `开源软件名称,开源软件版本号,开源软件的下载链接地址,是否对开源软件做出修改？,是否对开源软件进行了分发？
+`;
+  report += Object.entries(allDeps)
+    .filter(([packageName]) => {
+      return !/^(@cloudbase|@types|typescript)/.exec(packageName);
+    })
+    .map(([packageName, verison]) => {
+      let v = verison.replace('^', '');
+      return `${packageName},${v},https://www.npmjs.com/package/${packageName}/v/${v},否,是`;
+    })
+    .join('\n');
+
+  fs.writeFileSync('license-report.csv', report);
 }
 
-main();
-
+// 开源软件名称	开源软件版本号	开源软件的下载链接地址	是否对开源软件做出修改？	是否对开源软件进行了分发？
 async function licenseCheck(file, cwd) {
   console.log('\n', 'Check Package Lisense', file, '\n');
-  const licenseCommand = path.join(
-    __dirname,
-    '..',
-    'node_modules/.bin/license-checker'
+  const packageJSON = JSON.parse(
+    fs.readFileSync(path.join(cwd, 'package.json'))
   );
-  await spawnPromise(`${licenseCommand} --summary`, {
-    cwd,
-  });
+  const directDeps = packageJSON.dependencies;
+
+  Object.assign(allDeps, directDeps);
 }
