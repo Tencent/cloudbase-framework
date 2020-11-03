097b74c75510374505aa4ee662ba327af3e86e9a
Booker Zhao <bookerzhao@tencent.com>
2020-11-03 16:56:47 +0800

    fix(lint): fix lint issue

--- scripts/markdown-render.js
+++ scripts/markdown-render.js
@@ -46,7 +46,8 @@ ${data
   .map((item) => {
     return (
       `| <a href="${item.link}"><img width="200" src="${item.cover}"></a>  | [${item.npmPackageName}](${item.link}) ` +
-      `|[![Npm version](https://img.shields.io/npm/v/${item.npmPackageName})](https://www.npmjs.com/package/${item.npmPackageName})` +
+      `|[![Npm version](https://img.shields.io/npm/v/${item.npmPackageName})]` +
+      `(https://www.npmjs.com/package/${item.npmPackageName})` +
       ` | ${item.description}|`
     );
   })
