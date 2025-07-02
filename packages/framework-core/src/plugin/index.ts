cb921f16e34b9dcb94128882c9f1a90f861552aa
bookerzhao <bookerzhao@tencent.com>
2025-07-02 12:17:23 +0800

    chore(doc): update copyright

--- packages/framework-core/src/plugin/index.ts
+++ packages/framework-core/src/plugin/index.ts
@@ -1,7 +1,7 @@
 /**
  * Tencent is pleased to support the open source community by making CloudBaseFramework - 云原生一体化部署工具 available.
  *
- * Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+ * Copyright (C) 2025 Tencent.  All rights reserved.
  *
  * Please refer to license text included with this package for license details.
  */
